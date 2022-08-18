47335053a2252e346a2378e50c0dc34e6b2df42b
Ashwani Kumar <akb762@gmail.com>
2022-08-18 16:33:45 +0530

    Fixing

--- src/lib_gui/window.cc
+++ src/lib_gui/window.cc
@@ -22,7 +22,7 @@ Window::~Window() {
 
 void Window::loop() {
   while (!glfwWindowShouldClose(m_window)) {
-    glfwPollEvents();
+    glfwWaitEvents();
   }
 }
 
@@ -74,38 +74,6 @@ void Window::initGLFW() {
     }
   }
 
-  glfwSetMouseButtonCallback(
-      m_window, [](GLFWwindow* window, int button, int action, int mods) {
-        auto win = static_cast<Window*>(glfwGetWindowUserPointer(window));
-        if (action == GLFW_PRESS)
-          win->m_mouseButtonDown = true;
-        else if (action == GLFW_RELEASE)
-          win->m_mouseButtonDown = false;
-      });
-
-  glfwSetKeyCallback(m_window, [](GLFWwindow* window, int key, int scanCode,
-                                  int action, int mods) {
-    auto keyName = glfwGetKeyName(key, scanCode);
-    if (keyName) key = std::toupper(keyName[0]);
-
-    auto* win = static_cast<Window*>(glfwGetWindowUserPointer(window));
-
-    auto& io = ImGui::GetIO();
-
-    if (action == GLFW_PRESS || action == GLFW_REPEAT) {
-      win->m_pressedKeys.push_back(key);
-      io.KeysDown[key] = true;
-      io.KeyCtrl = (mods & GLFW_MOD_CONTROL) != 0;
-      io.KeyShift = (mods & GLFW_MOD_SHIFT) != 0;
-      io.KeyAlt = (mods & GLFW_MOD_ALT) != 0;
-    } else if (action == GLFW_RELEASE) {
-      io.KeysDown[key] = false;
-      io.KeyCtrl = (mods & GLFW_MOD_CONTROL) != 0;
-      io.KeyShift = (mods & GLFW_MOD_SHIFT) != 0;
-      io.KeyAlt = (mods & GLFW_MOD_ALT) != 0;
-    }
-  });
-
   glfwShowWindow(m_window);
 }
 
@@ -124,30 +92,6 @@ void Window::initImGUI() {
       ImGuiConfigFlags_NavEnableKeyboard;
 
     for (auto& entry : fonts->ConfigData) io.Fonts->ConfigData.push_back(entry);
-
-  //io.ConfigViewportsNoTaskBarIcon = false;
-  io.KeyMap[ImGuiKey_Tab] = GLFW_KEY_TAB;
-  io.KeyMap[ImGuiKey_LeftArrow] = GLFW_KEY_LEFT;
-  io.KeyMap[ImGuiKey_RightArrow] = GLFW_KEY_RIGHT;
-  io.KeyMap[ImGuiKey_UpArrow] = GLFW_KEY_UP;
-  io.KeyMap[ImGuiKey_DownArrow] = GLFW_KEY_DOWN;
-  io.KeyMap[ImGuiKey_PageUp] = GLFW_KEY_PAGE_UP;
-  io.KeyMap[ImGuiKey_PageDown] = GLFW_KEY_PAGE_DOWN;
-  io.KeyMap[ImGuiKey_Home] = GLFW_KEY_HOME;
-  io.KeyMap[ImGuiKey_End] = GLFW_KEY_END;
-  io.KeyMap[ImGuiKey_Insert] = GLFW_KEY_INSERT;
-  io.KeyMap[ImGuiKey_Delete] = GLFW_KEY_DELETE;
-  io.KeyMap[ImGuiKey_Backspace] = GLFW_KEY_BACKSPACE;
-  io.KeyMap[ImGuiKey_Space] = GLFW_KEY_SPACE;
-  io.KeyMap[ImGuiKey_Enter] = GLFW_KEY_ENTER;
-  io.KeyMap[ImGuiKey_Escape] = GLFW_KEY_ESCAPE;
-  io.KeyMap[ImGuiKey_KeyPadEnter] = GLFW_KEY_KP_ENTER;
-  io.KeyMap[ImGuiKey_A] = GLFW_KEY_A;
-  io.KeyMap[ImGuiKey_C] = GLFW_KEY_C;
-  io.KeyMap[ImGuiKey_V] = GLFW_KEY_V;
-  io.KeyMap[ImGuiKey_X] = GLFW_KEY_X;
-  io.KeyMap[ImGuiKey_Y] = GLFW_KEY_Y;
-  io.KeyMap[ImGuiKey_Z] = GLFW_KEY_Z;
 
   {
     GLsizei width, height;
